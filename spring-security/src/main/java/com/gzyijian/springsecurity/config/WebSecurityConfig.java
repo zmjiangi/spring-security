b41e5249111e1f038457c56a93c6abbfb1f49854
zmjiangi <zmjiangi@qq.com>
2018-05-26 16:31:22 +0800

    记住我实现

--- spring-security/src/main/java/com/gzyijian/springsecurity/config/WebSecurityConfig.java
+++ spring-security/src/main/java/com/gzyijian/springsecurity/config/WebSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.gzyijian.springsecurity.config;
 
 import com.gzyijian.springsecurity.filter.ValidateCodeFilter;
+import com.gzyijian.springsecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * Created by zmjiangi on 2018-5-11.
@@ -14,7 +20,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private UserService userService;
+    @Autowired
     private ValidateCodeFilter validateCodeFilter;
+
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+//        jdbcTokenRepository.setCreateTableOnStartup(true);
+        return jdbcTokenRepository;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,6 +50,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .failureUrl("/login?error")
+                .and()
+                .rememberMe()
+                .tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userService)
                 .and()
                 .logout()
                 .logoutUrl("/logout")
--- spring-security/src/main/webapp/WEB-INF/jsp/login.jsp
+++ spring-security/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -9,6 +9,7 @@
     密码：<input type="password" name="password"/><br/><br/>
     验证码：<input type="text" name="imageCode"/>
     <img src="/api/validateCode" alt="验证码"/><br/><br/>
+    <input type="checkbox" name="remember-me" value="true"/>记住我
     <input type="submit" value="登录"/>
 </form>
 </body>
